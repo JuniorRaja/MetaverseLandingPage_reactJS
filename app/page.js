9b81a29b7ab40c85f055c538af5a51a8768889e8
Prasanna Rajendran <imprasannarajendran@gmail.com>
2022-12-09 08:48:07 +0530

    About Section Fin.

--- app/page.js
+++ app/page.js
@@ -14,13 +14,28 @@ const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-    <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0">
+        <Explore />
+      </div>
+    </div>
+
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0">
+        <WhatsNew />
+      </div>
+    </div>
+
     <World />
-    <Insights />
-    <Feedback />
+
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0">
+        <Feedback />
+      </div>
+    </div>
     <Footer />
   </div>
 );
