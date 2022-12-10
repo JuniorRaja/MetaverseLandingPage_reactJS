f7e33cde3db78b433e51692ee182ec2c0c646fc0
Prasanna Rajendran <imprasannarajendran@gmail.com>
2022-12-10 10:21:12 +0530

    Explore Section Fin.

--- app/page.js
+++ app/page.js
@@ -16,25 +16,22 @@ const Page = () => (
     <Hero />
     <div className="relative">
       <About />
-      <div className="gradient-03 z-0">
-        <Explore />
-      </div>
+      <div className="gradient-03 z-0" />
+      <Explore />
     </div>
 
     <div className="relative">
       <GetStarted />
-      <div className="gradient-04 z-0">
-        <WhatsNew />
-      </div>
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
     </div>
 
     <World />
 
     <div className="relative">
       <Insights />
-      <div className="gradient-04 z-0">
-        <Feedback />
-      </div>
+      <div className="gradient-04 z-0" />
+      <Feedback />
     </div>
     <Footer />
   </div>
